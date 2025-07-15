6842f3570ab88c6655d4123ca98c782357e2b33a
TaliesenKing <taliesenking@yahoo.com>
2025-07-14 20:41:03 -0700

    likes backend in progress

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,8 @@
-# If adding more models we need to import them her in the init file
+# If adding more models we need to import them here in the init file
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
+from .like import Like #added by TJ
+from .post import Post #added by TJ
+
+__all__ = ['db', 'User', 'Post', 'Like']#added by TJ
---
+++ app/models/like.py
@@ -0,0 +1,15 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Like(db.Model):
+    __tablename__ = 'likes'#we can change the name if we need to
+
+    #adding boolean to account for env
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+    #ids
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("posts.id")), nullable=False)
+    #relationships
+    user = db.relationship("User", back_populates="likes")
+    post = db.relationship("Post", back_populates="likes")
