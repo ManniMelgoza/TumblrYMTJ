3066a640191eb3acbeb28dcb224ac2d0a025b7af
jaacobbbb20 <jhathaway2002@icloud.com>
2025-07-18 12:38:21 -0700

    Trying to work through GIT issues on work computer-TEST COMMIT

--- app/models/user.py
+++ app/models/user.py
@@ -2,6 +2,7 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 # where is this user class coming in and worry about user class
 from flask_login import UserMixin
+from .follow import Follow
 
 
 class User(db.Model, UserMixin):
@@ -15,6 +16,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
     likes = db.relationship("Like", back_populates="user", cascade="all, delete-orphan")
+    following = db.relationship("Follow", foreign_keys=[Follow.follower_id], back_populates="follower", cascade="all, delete-orphan")
+    followers = db.relationship("Follow", foreign_keys=[Follow.following_id], back_populates="following", cascade="all, delete-orphan")
     
     @property
     def password(self):
@@ -31,5 +34,7 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'followers_count': len(self.followers),
+            'following_count': len(self.following),
         }
