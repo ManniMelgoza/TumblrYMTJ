15e80a813c37c4fe7619125205e6142451eefba4
jaacobbbb20 <jhathaway.1@icloud.com>
2025-07-18 17:30:03 -0700

    Revert "REVERT"
    This reverts commit c1537e1f697f8b3e3f96b43f6ae67a0920db5a62.

--- app/models/follow.py
+++ app/models/follow.py
@@ -10,16 +10,8 @@ class Follow(db.Model):
     # IDs
     id = db.Column(db.Integer, primary_key=True)
     follower_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
-    following_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
+    followed_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
 
     # Relationships
     follower = db.relationship("User", foreign_keys=[follower_id], back_populates="following")
-    following = db.relationship("User", foreign_keys=[following_id], back_populates="followers")
-
-
-    # set a to_dict for the following 
-     def to_dict(self):
-        return {
-            'following_id': self.following_id,
-
-        }
+    followed = db.relationship("User", foreign_keys=[followed_id], back_populates="followers")
