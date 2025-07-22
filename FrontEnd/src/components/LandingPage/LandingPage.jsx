e8e68945460a2974c7df70ae45358be1b00e871f
Emmanuel <emmanuelmelgoza1@gmail.com>
2025-07-21 18:27:47 -0600

    Merge: Post FrontEnd to Dev

--- FrontEnd/src/components/LandingPage/LandingPage.jsx
+++ FrontEnd/src/components/LandingPage/LandingPage.jsx
@@ -22,37 +22,34 @@ const LandingPage = () => {
         <>
              {/* Logo -> Explore -> Sign Up and Log In button -> Column Div */}
             <div>
-                <h1>LOGO</h1>
-                </div>
-            {/* Title and Create Post Button -> Horizontal Div */}
-            <div>
-                <h1>Browse Posts</h1>
-                {postsArr.map((post) => (
-                    <div key={post.id} className="post-card">
-                    <h2>{post.post_title}</h2>
-                    <p>{post.post_body}</p>
-                    {post.post_img_url && <img src={post.post_img_url} alt={post.post_title} />}
-                    </div>
-                ))}
-                <button>Create a Post</button>
+
             </div>
+
+            {/* Title and Create Post Button -> Horizontal Div */}
+            <h1>Browse Posts</h1>
+            <button>Create a Post</button>
 
             {/* IMAGE DISPLAY BLOCKs */}
             {/* IMAGE WRAPPER */}
             <div>
-
                 {/* Main Large Post Left */}
                 <div>
-                    <h1>main image</h1>
+                    <h1>Main Image</h1>
                 </div>
-
                 {/* Display small posts 2 Rows and 3 post per Row */}
                 <div>
                     <h1>Small images</h1>
+                        <div className='grid'>
+                            {postsArr.slice(0, 6).map((post) => (
+                                <div key={post.id} className="post-card">
+                                {/* <h2>{post.post_title}</h2> */}
+                                {/* <p>{post.post_body}</p> */}
+                                {<img src={post.post_img_url} alt={post.post_title} />}
+                                </div>
+                            ))}
+                        </div>
                 </div>
-
             </div>
-
         </>
     )
 }
