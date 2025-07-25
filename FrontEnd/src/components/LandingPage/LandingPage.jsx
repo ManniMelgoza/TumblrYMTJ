5c4a2fe6f9821910d797a22526247407e0eb4d0c
Emmanuel <emmanuelmelgoza1@gmail.com>
2025-07-24 19:00:25 -0600

    FrontEnd: Added css to the landing page and got the main image to be selected random on page refresh

--- FrontEnd/src/components/LandingPage/LandingPage.jsx
+++ FrontEnd/src/components/LandingPage/LandingPage.jsx
@@ -5,6 +5,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { thunkGetAllPosts } from "../../redux/post";
 import './LandingPage.css';
 import { useEffect } from "react";
+import { FaRegCompass } from "react-icons/fa";
+
+
 
 const LandingPage = () => {
     const dispatch = useDispatch();
@@ -18,27 +21,43 @@ const LandingPage = () => {
         dispatch(thunkGetAllPosts());
     }, [dispatch]);
 
+// Random Image Selector Section
+    const postImagesList = postsArr.map(post => post.post_img_url).filter(Boolean);
+    console.log('POST IMG LIST', postImagesList)
+
+    const randomIndex = Math.floor(Math.random() * postsArr.length);
+    console.log('Rand Indx', randomIndex)
+    const selectedImage = postImagesList[randomIndex];
+    console.log('SELECTED IMG', selectedImage)
+
     return (
         <>
+            <h1 className="browse-post-title">Browse Posts</h1>
+        <div className="landing-container">
              {/* Logo -> Explore -> Sign Up and Log In button -> Column Div */}
-            <div>
-
-            </div>
-
+            <div className = "logoColumn">
             {/* Title and Create Post Button -> Horizontal Div */}
-            <h1>Browse Posts</h1>
+            <FaRegCompass /> <button>Explore</button>
             <button>Create a Post</button>
+            </div>
 
             {/* IMAGE DISPLAY BLOCKs */}
             {/* IMAGE WRAPPER */}
-            <div>
+            <div className="image-wrapper">
                 {/* Main Large Post Left */}
-                <div>
-                    <h1>Main Image</h1>
+                <div className="mainImgSection">
+                    {/* <h1>Main Image</h1> */}
+                    <div className ='mainGridImg'>
+                        <div className ='mainImg-card'>
+                            {
+                                <img src={ selectedImage } alt={ postsArr.post_title } />
+                            }
+                        </div>
+                    </div>
                 </div>
                 {/* Display small posts 2 Rows and 3 post per Row */}
-                <div>
-                    <h1>Small images</h1>
+                <div className="smallImgSection">
+                    {/* <h1>Small images</h1> */}
                         <div className='grid'>
                             {postsArr.slice(0, 6).map((post) => (
                                 <div key={post.id} className="post-card">
@@ -50,6 +69,7 @@ const LandingPage = () => {
                         </div>
                 </div>
             </div>
+        </div>
         </>
     )
 }
